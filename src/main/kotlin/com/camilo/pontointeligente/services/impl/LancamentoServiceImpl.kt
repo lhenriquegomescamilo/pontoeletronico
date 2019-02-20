94ecbaaf4e191b241aa654160884cb891f8d21dd
lcamilo <lhenriquegomescamilo@gmail.com>
2019-02-19 22:51:30 -0300

    Implementando testes para validar o funcionamento do serviço de lancamento

--- src/main/kotlin/com/camilo/pontointeligente/services/impl/LancamentoServiceImpl.kt
+++ src/main/kotlin/com/camilo/pontointeligente/services/impl/LancamentoServiceImpl.kt
@@ -3,23 +3,17 @@ package com.camilo.pontointeligente.services.impl
 import com.camilo.pontointeligente.documents.Lancamento
 import com.camilo.pontointeligente.repositories.LancamentoRepository
 import com.camilo.pontointeligente.services.LancamentoService
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 
 @Service
 class LancamentoServiceImpl(val lancamentoRepository: LancamentoRepository) : LancamentoService {
-    override fun buscarPorFuncionarioId(funcionarioId: String, pageRequest: PageRequest): Page<Lancamento> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun buscarPorFuncionarioId(funcionarioId: String, pageRequest: PageRequest) = lancamentoRepository
+            .findByFuncionarioId(funcionarioId, pageRequest)
 
-    override fun buscarPorId(id: String): Lancamento? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun buscarPorId(id: String): Lancamento? = lancamentoRepository.findOne(id)
 
     override fun persistir(lancamento: Lancamento) = lancamentoRepository.save(lancamento)
 
-    override fun removerPorId(id: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun removerPorId(id: String) = lancamentoRepository.delete(id)
 }
--- src/test/kotlin/com/camilo/pontointeligente/services/LancamentoServiceTest.kt
+++ src/test/kotlin/com/camilo/pontointeligente/services/LancamentoServiceTest.kt
@@ -4,6 +4,7 @@ import com.camilo.pontointeligente.documents.Lancamento
 import com.camilo.pontointeligente.enums.TipoEnum
 import com.camilo.pontointeligente.repositories.LancamentoRepository
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,6 +13,8 @@ import org.mockito.Mockito.any
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
 import org.springframework.test.context.junit4.SpringRunner
 import java.util.*
 
@@ -26,10 +29,16 @@ class LancamentoServiceTest {
     @Autowired
     val lancamentoService: LancamentoService? = null
 
+    private val ID = "1"
+
     @Before
     @Throws(Exception::class)
     fun setUp() {
         given(lancamentoRepository?.save(any(Lancamento::class.java))).willReturn(lancamento())
+        given(lancamentoRepository?.findOne(ID)).willReturn(lancamento())
+        given(lancamentoRepository?.findByFuncionarioId(ID, PageRequest(1, 10)))
+                .willReturn(PageImpl(listOf(lancamento())))
+
     }
 
     @Test
@@ -37,6 +46,19 @@ class LancamentoServiceTest {
         val lancamento = lancamentoService?.persistir(lancamento())
         assertNotNull(lancamento)
 
+    }
+
+    @Test
+    fun deve_buscar_por_id() {
+        val lancamento = lancamentoService?.buscarPorId(ID)
+        assertNotNull(lancamento)
+    }
+
+    @Test
+    fun deve_buscar_todos_lancamento_por_funcionario_id() {
+        val pageLancamentos = lancamentoService?.buscarPorFuncionarioId(ID, PageRequest(1, 10))
+        assertNotNull(pageLancamentos)
+        assertNotNull(pageLancamentos?.first())
     }
 
     private fun lancamento(): Lancamento {
